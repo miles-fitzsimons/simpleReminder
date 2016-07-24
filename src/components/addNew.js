43e7a390b8cd1f8bff6eb06a91b35037258c1e66
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-07-25 10:53:16 +1200

    mindate working

--- src/components/addNew.js
+++ src/components/addNew.js
@@ -2,36 +2,76 @@ import React, { Component } from 'react'
 import {
   View,
   Text,
-  TextInput
+  TextInput,
+  TouchableHighlight,
+  StyleSheet,
+  Picker
 } from 'react-native'
+import DatePicker from 'react-native-datepicker'
+import moment from 'moment'
+
 
 class AddNew extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      reminderText: ''
-    }
-  }
+    // Set default reminder day/time to tomorrow at 6:00pm
+    let today = new Date()
+    console.log('date', today)
 
-  blur(text) {
-    console.log('text')
+    // let day = dateNow.getDate()
+    // today.setDate(today.getDate() + 1)
+    console.log('day', today)
+
+    this.state = {
+      reminderText: '',
+      datetime: today
+    }
   }
 
   render() {
     // Figure out new key id here?
-    console.log('add new props', this.blur)
+    console.log('add new props', this.state)
     return (
-      <View>
-        <Text>What</Text>
+      <View style={styles.container}>
         <TextInput
-          id="what"
+          style={styles.textInput}
           placeholder="Remind me to..."
-          onBlur={this.blur}
+          onChangeText={(text) => {this.setState({reminderText: text})}}
         />
-        <Text>When</Text>
+        <Text>at</Text>
+        <DatePicker
+          style={{width: 200}}
+          date={this.state.datetime}
+          format="DD-MM-YYYY HH:mm"
+          minDate={moment().format('DD MM YYYY')}
+          mode="datetime"
+          showIcon={true}
+          onDateChange={(datetime) => {this.setState({datetime: datetime});}}
+        />
+        <TouchableHighlight
+          onPress={() => {console.log('hi')}}
+          style={styles.saveButton}
+        >
+          <Text>Save</Text>
+        </TouchableHighlight>
+
       </View>
     )
   }
 }
 
 export default AddNew
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  saveButton: {
+    backgroundColor: 'red'
+  },
+  textInput: {
+    width: 250
+  }
+})
